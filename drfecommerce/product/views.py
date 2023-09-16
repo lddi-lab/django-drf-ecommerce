682a3bf3e5232200e6152ac9de140432a445965d
Lucky Icban <lddicban@gmail.com>
2023-09-17 06:53:20 +0800

    Build: Category Filter and Extra Actions

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Brand, Category, Product
@@ -33,7 +33,7 @@ class BrandViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
 
-class ProductViewSet(viewsets.ViewSet):
+class ProductViewSet(viewsets.ModelViewSet):
     """
     A simple Viewset for viewing all products
     """
@@ -43,4 +43,19 @@ class ProductViewSet(viewsets.ViewSet):
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all",
+    )
+    def list_product_by_category(self, request, category=None):
+        """
+        An endpoint to return products by category
+        """
+
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True
+        )
         return Response(serializer.data)
