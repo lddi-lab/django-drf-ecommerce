9ab519f718872964850fdcfd23723c46fa6c1d12
Lucky Icban <lddicban@gmail.com>
2023-09-17 05:07:55 +0800

    APIClient End-to-End Testing

--- drfecommerce/tests/conftest.py
+++ drfecommerce/tests/conftest.py
@@ -1,11 +1,14 @@
-import os
-import sys
-
 import pytest
 from pytest_factoryboy import register
+from rest_framework.test import APIClient
 
 from .factories import BrandFactory, CategoryFactory, ProductFactory
 
 register(CategoryFactory)
 register(BrandFactory)
 register(ProductFactory)
+
+
+@pytest.fixture
+def api_client():
+    return APIClient
