49cf8efd68f9c4edbab39562a4f8e1b083fe0989
Lucky Icban <lddicban@gmail.com>
2023-09-19 00:35:21 +0800

    Build: Editing Multiple Models in the Django Admin Site

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -38,7 +38,7 @@ class Product(models.Model):
 class ProductLine(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5)
     sku = models.CharField(max_length=100)
-    stock_qty = models.IntegerField
+    stock_qty = models.IntegerField()
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
