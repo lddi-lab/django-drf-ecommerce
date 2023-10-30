169c8147b0b7b1602171ca60b0c81295cdc9dcfe
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2023-10-30 08:40:20 +0800

    Build: Implementing Product Type Tables

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -98,6 +98,7 @@ class ProductLine(models.Model):
         through="ProductLineAttributeValue",
         related_name="product_line_attribute_value",
     )
+    product_type = models.ForeignKey("ProductType", on_delete=models.PROTECT)
     objects = ActiveOueryset.as_manager()
 
     def clean(self, exclude=None):
@@ -134,3 +135,23 @@ class ProductImage(models.Model):
 
     def __str__(self):
         return str(self.url)
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class ProductTypeAttribute(models.Model):
+    product_type = models.ForeignKey(
+        ProductType,
+        on_delete=models.CASCADE,
+        related_name="product_type_attribute_pt",
+    )
+    attribute = models.ForeignKey(
+        Attribute,
+        on_delete=models.CASCADE,
+        related_name="product_type_attribute_a",
+    )
+
+    class Meta:
+        unique_together = ("product_type", "attribute")
