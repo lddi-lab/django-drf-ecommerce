45e10f6ab2f02c8d8c9358e0d4c07c1c102fc1af
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2023-11-01 13:06:00 +0800

    Build: Customizing the Serialization Output

--- drfecommerce/product/serializers.py
+++ drfecommerce/product/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
-from .models import Brand, Category, Product, ProductImage, ProductLine
+from .models import (
+    Brand,
+    Category,
+    Product,
+    ProductImage,
+    ProductLine,
+    Attribute,
+    AttributeValue,
+)
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -23,8 +31,25 @@ class ProductImageSerializer(serializers.ModelSerializer):
         exclude = ("id", "productline")
 
 
+class AttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attribute
+        fields = ("name", "id")
+
+
+class AttributeValueSerializer(serializers.ModelSerializer):
+    attribute = AttributeSerializer(many=False)
+
+    class Meta:
+        model = AttributeValue
+        fields = (
+            "attribute",
+            "attribute_value",
+        )
+
+
 class ProductLineSerializer(serializers.ModelSerializer):
-    product_image = ProductImageSerializer(many=True)
+    attribute_value = AttributeValueSerializer(many=True)
 
     class Meta:
         model = ProductLine
@@ -34,7 +59,17 @@ class ProductLineSerializer(serializers.ModelSerializer):
             "stock_qty",
             "order",
             "product_image",
+            "attribute_value",
         )
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        av_data = data.pop("attribute_value")
+        attr_values = {}
+        for key in av_data:
+            attr_values.update({key["attribute"]["id"]: key["attribute_value"]})
+        data.update({"specification": attr_values})
+        return data
 
 
 class ProductSerializer(serializers.ModelSerializer):
